eeafbad070397fcca21d5d7097ac11b7be667a27
Stiven Agudeloo <sagudeloo@eafit.edu.co>
2021-01-14 02:32:00 -0500

    Annadido la forma para crear canales con el comando !private-room

--- src/index.js
+++ src/index.js
@@ -1,40 +1,46 @@
-const { Client, MessageEmbed, Message} = require('discord.js');
+const { Client, MessageEmbed} = require('discord.js');
 const client = new Client();
 
 client.on('ready', () => {
-    console.log(`Bot is ready as ${client.user.tag}!`);
+    console.log(`Bot is ready as ${client.user.tag}.`);
+
 });
+
+client.on('debug', console.log)
 
 client.on('message', async msg => {
     console.log(`Bot recive: [${msg.content}], from: [${msg.author}]` )
-    if ( msg.content === 'ping'){
-        msg.reply('Pong!');
-    }
-    if (msg.content === '!test'){+
-        msg.delete()
-        msg.channel.send('glad your are testing');
-    }
     if (msg.content === '!dev'){
         msg.channel.send('https://www.sagudeloo.com');
     }
-    if (msg.content === 'Hermana' || msg.content === 'hermana'){
-        msg.channel.send('Hola Salomé');
-    }
-    if (msg.content === '!pretty'){
-        const embed = new MessageEmbed()
-            .setTitle('A pretty messege')
-            .setColor('RED')
-            .setAuthor('Stiven Agudelo', 'https://cdn.awsli.com.br/31/31979/arquivos/gato-quem-somos.png')
-            // .addField('Something One', 'Lorem Impusun')
-            // .addField('Something One', 'Lorem Impusun')
-            // .addField('Something One', 'Lorem Impusun')
-            // .addField('Something One', 'Lorem Impusun');
-        msg.channel.send(embed);
-    }
     if (msg.content === '!clear'){
-        const fetched = await msg.channel.messages.fetch({limit:100});
-        msg.channel.bulkDelete(fetched)
+        const fetchedMesseges = await msg.channel.messages.fetch({limit:100});
+        msg.channel.bulkDelete(fetchedMesseges)
         console.log('Mensajes eliminados')
+    }
+    if (msg.content === '!private-room'){
+        if ( msg.guild.available ){
+            const channels = await msg.guild.channels.cache;
+            privateRoomParent = null;
+            // Check if PRIVATE ROOMS category exist
+            // And if exist then assing thant to privateRoomParent variable
+            channels.forEach((channel, _) => {
+                if (channel.type === 'category' && channel.name === 'PRIVATE ROOMS'){
+                    privateRoomParent = channel;
+                    console.log("Eureca tenemos un PRIVATE ROOMS")
+                }
+            });
+            // Create a PRIVATE ROOMS category if it didn't exist
+            if (!privateRoomParent){
+                privateRoomParent = await msg.guild.channels.create('PRIVATE ROOMS', {
+                    "type": 'category'
+                });
+            }
+            await msg.guild.channels.create('private room',{
+                "type": 'text',
+                "parent": privateRoomParent
+            });
+        }
     }
 });
 
